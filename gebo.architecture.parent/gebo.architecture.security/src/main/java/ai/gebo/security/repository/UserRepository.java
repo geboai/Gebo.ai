6ea884950c9203c2ef2fa05bb6f3de6455fc2aeb
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-05-26 19:29:32 +0200

    integration testing adaptation

--- gebo.architecture.parent/gebo.architecture.security/src/main/java/ai/gebo/security/repository/UserRepository.java
+++ gebo.architecture.parent/gebo.architecture.security/src/main/java/ai/gebo/security/repository/UserRepository.java
@@ -6,12 +6,10 @@
  * and https://mozilla.org/MPL/2.0/.
  * Copyright (c) 2025+ Gebo.ai 
  */
- 
- 
- 
 
 package ai.gebo.security.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Example;
@@ -23,58 +21,98 @@ import org.springframework.stereotype.Repository;
 import ai.gebo.security.model.User;
 
 /**
- * Gebo.ai comment agent
- * UserRepository interface for accessing User data in a MongoDB database.
- * Extends MongoRepository to provide basic CRUD operations on User collection.
+ * Gebo.ai comment agent UserRepository interface for accessing User data in a
+ * MongoDB database. Extends MongoRepository to provide basic CRUD operations on
+ * User collection.
  */
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-	
+
 	/**
-	 * Interface representing a subset of User information.
-	 * Typically used for projections.
+	 * Interface representing a subset of User information. Typically used for
+	 * projections.
 	 */
 	public static interface UserInfos {
 		/**
 		 * Gets the username of the user.
+		 * 
 		 * @return The username.
 		 */
 		String getUsername();
-		
+
 		/**
 		 * Gets the name of the user.
+		 * 
 		 * @return The user's name.
 		 */
 		String getName();
 
 		/**
 		 * Gets the sourname of the user.
+		 * 
 		 * @return The user's surname.
 		 */
 		String getSourname();
 
 		/**
 		 * Indicates if the user is disabled.
+		 * 
 		 * @return True if disabled, false otherwise.
 		 */
 		Boolean getDisabled();
 
 		/**
 		 * Retrieves the roles associated with the user.
+		 * 
 		 * @return A list of roles.
 		 */
 		java.util.List<String> getRoles();
+
+		public static UserInfos of(final User user) {
+			return new UserInfos() {
+				@Override
+				public Boolean getDisabled() {
+					
+					return user.getDisabled();
+				}
+
+				@Override
+				public String getUsername() {
+					
+					return user.getUsername();
+				}
+
+				@Override
+				public String getName() {
+					return user.getName();
+				}
+
+				@Override
+				public String getSourname() {
+					return user.getSourname();				}
+
+				@Override
+				public List<String> getRoles() {
+					
+					return user.getRoles();
+				}
+			};
+			
+		}
+
 	}
 
 	/**
 	 * Finds a User by their username.
+	 * 
 	 * @param email The username to search for.
 	 * @return An Optional containing the found User, or empty if not found.
 	 */
 	Optional<User> findByUsername(String email);
-	
+
 	/**
 	 * Finds Users by their roles.
+	 * 
 	 * @param role The role to search for.
 	 * @return A list of Users with the specified role.
 	 */
@@ -82,6 +120,7 @@ public interface UserRepository extends MongoRepository<User, String> {
 
 	/**
 	 * Checks if a User exists by their username.
+	 * 
 	 * @param email The username to check.
 	 * @return True if a User with the username exists, false otherwise.
 	 */
@@ -89,7 +128,8 @@ public interface UserRepository extends MongoRepository<User, String> {
 
 	/**
 	 * Finds a pageable list of UserInfos that match the given example.
-	 * @param example An example User for querying.
+	 * 
+	 * @param example  An example User for querying.
 	 * @param pageable The pagination information.
 	 * @return A page of UserInfos.
 	 */
