dd00026c3f6f52d9743edb63e1f9796fe40bc47e
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-07-06 10:59:03 +0200

    update on docker image security and added sbom

--- dockers/gebo.ai/create-image.bat
+++ dockers/gebo.ai/create-image.bat
@@ -1,3 +1,4 @@
 xcopy ..\..\gebo.apps.parent\gebo.ai.app\target\gebo.ai.app-0.9.9.7-SNAPSHOT-bootable.jar .
+xcopy ..\..\gebo.apps.parent\gebo.ai.app\target\classes\META-INF\sbom\application.cdx.json
 docker image rm geboai/gebo.ai --force
 docker build --build-arg JAVA_EXTRA_SECURITY_DIR=/opt/gebo.ai -t geboai/gebo.ai -t geboai/gebo.ai:0.9.9.7-SNAPSHOT .
