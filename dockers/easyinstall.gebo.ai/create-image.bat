b5f332378c71f6658fcf58deff39583f9ea4bd1c
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-09-01 10:47:39 +0200

    Changed version from 0.9.9.7-SNAPSHOT to 0.9.9.7

--- dockers/easyinstall.gebo.ai/create-image.bat
+++ dockers/easyinstall.gebo.ai/create-image.bat
@@ -1,4 +1,4 @@
-xcopy ..\..\gebo.apps.parent\gebo.ai.app\target\gebo.ai.app-0.9.9.7-SNAPSHOT-bootable.jar .
+xcopy ..\..\gebo.apps.parent\gebo.ai.app\target\gebo.ai.app-0.9.9.7-bootable.jar .
 xcopy ..\..\gebo.apps.parent\gebo.ai.app\target\classes\META-INF\sbom\application.cdx.json
 docker image rm geboai/easyinstall.gebo.ai --force
-docker build --build-arg JAVA_EXTRA_SECURITY_DIR=/opt/gebo.ai -t geboai/easyinstall.gebo.ai -t geboai/easyinstall.gebo.ai:0.9.9.7-SNAPSHOT .
+docker build --build-arg JAVA_EXTRA_SECURITY_DIR=/opt/gebo.ai -t geboai/easyinstall.gebo.ai -t geboai/easyinstall.gebo.ai:0.9.9.7 .
