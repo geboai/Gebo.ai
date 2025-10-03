7ff2bc01392e326e55fab62bce2a38fdc22406bc
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-10-03 19:11:02 +0200

    Trying to fix neo4j connectivity in easyinstall.gebo.ai docker image

--- dockers/easyinstall.gebo.ai/gebo.ai.sh
+++ dockers/easyinstall.gebo.ai/gebo.ai.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 wait_for() { # host port timeout
-  local host="$1" port="$2" timeout="${3:-60}" t=0
-  echo "Waiting for $host:$port up to ${timeout}s..."
+  local host="$1" port="$2"  timeout="${3:-60}" serv="$4" t=0
+  echo "Waiting for service $serv $host:$port up to ${timeout}s..."
   until (echo > /dev/tcp/$host/$port) >/dev/null 2>&1; do
-    sleep 1; t=$((t+1)); [ $t -ge $timeout ] && return 1
+    sleep 1; 
+    t=$((t+1)); 
+    if [ "$t" -ge "$timeout" ]; then
+      echo "Timeout waiting for $serv on $host:$port"
+      return 1
+    fi
   done
+  echo "$serv is up."
 }
 
 # Examples (same container network):
-wait_for 127.0.0.1 27017 30   || { echo "Mongo not ready"; exit 1; }
-wait_for 127.0.0.1 6333  30   || { echo "Qdrant not ready"; exit 1; }
-wait_for 127.0.0.1 7687  30   || { echo "Neo4j (bolt) not ready"; exit 1; }
+wait_for 127.0.0.1 27017 90 MongoDB   || { echo "Mongo not ready"; exit 1; }
+wait_for 127.0.0.1 6333  90 QDrant  || { echo "Qdrant not ready"; exit 1; }
+wait_for 127.0.0.1 7687  90 Neo4j   || { echo "Neo4j (bolt) not ready"; exit 1; }
 
 exec java $JAVA_TOOL_OPTIONS -jar /opt/gebo.ai/gebo.ai.app-0.9.9.8-SNAPSHOT-bootable.jar
--- dockers/easyinstall.gebo.ai/neo4j-entry.sh
+++ dockers/easyinstall.gebo.ai/neo4j-entry.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-export AUTH_FILE="/var/lib/neo4j/data/dbms/auth"
+export AUTH_FILE="/var/lib/neo4j/data/dbms/auth.ini"
 export  JAVA_TOOL_OPTIONS="-Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 --add-opens=java.base/java.nio.charset=ALL-UNNAMED --enable-native-access=ALL-UNNAMED  -Xmx1g -XX:+AggressiveHeap  -XX:+PrintGCDetails -Xlog:gc:/var/log/neo4j/gc.log"
 
 if [ ! -f "\$AUTH_FILE" ]; then
   echo "Setting initial Neo4j password..."
-  neo4j-admin dbms set-initial-password "\${NEO4J_PASSWORD:-neo4jmaster}" || true
+  neo4j-admin dbms set-initial-password "neo4j" 
+  cypher-shell -u neo4j -p neo4j "ALTER CURRENT USER SET PASSWORD CHANGE NOT REQUIRED;"
+  echo "Setting initial Neo4j password DONE!"
+  else 
+  echo "Already existing $AUTH_FILE"
 fi
 exec /usr/bin/neo4j console
