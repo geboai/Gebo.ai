28d58ba76e66aa44fd3fd121e40a9c9ad6299bd2
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-10-13 11:39:03 +0200

    UI: wrapper component implemented for standard primeng form control block

--- gebo.ui/projects/gebo-ai-reusable-ui/src/lib/controls/field-container/field-container.component.ts
+++ gebo.ui/projects/gebo-ai-reusable-ui/src/lib/controls/field-container/field-container.component.ts
@@ -1,6 +1,7 @@
-import { Component, Inject, Input, OnInit, Optional } from "@angular/core";
+import { Component, Inject, Input, OnChanges, OnInit, Optional, SimpleChanges } from "@angular/core";
 import { ControlContainer, FormGroupDirective } from "@angular/forms";
 import { GEBO_AI_FIELD_HOST, GeboAIFieldHost } from "../field-host-component-iface/field-host-component-iface";
+import { Subject } from "rxjs";
 
 @Component({
     templateUrl: "field-container.component.html",
@@ -11,25 +12,33 @@ import { GEBO_AI_FIELD_HOST, GeboAIFieldHost } from "../field-host-component-ifa
         { provide: ControlContainer, useExisting: FormGroupDirective }
     ]
 })
-export class GeboAIFieldContainerComponent implements OnInit{
-
+export class GeboAIFieldContainerComponent implements OnInit, OnChanges {
     @Input({ required: true }) id!: string;
-
-
     @Input({ required: true }) label!: string;
-
     @Input() placeholder!: string;
-
     @Input() help: string = '';
-
-
     @Input() required: boolean = false;
-    computedRequired = false;
-    constructor(@Optional() @Inject(GEBO_AI_FIELD_HOST) private host?: GeboAIFieldHost) { 
+    internalLabel!: string;
+    internalPlaceholder!: string;
+    internalHelp!: string;
+    computedRequired: boolean = false;
+    constructor(@Optional() @Inject(GEBO_AI_FIELD_HOST) private host?: GeboAIFieldHost) {
 
     }
+    localizationSubject:Subject<{label?:string,help?:string,placeholder?:string}>=new Subject(); 
+    ngOnChanges(changes: SimpleChanges): void {
+        if (!this.internalLabel && this.label && changes["label"]) {
+            this.internalLabel = this.label;
+        }
+        if (!this.internalHelp && this.help && changes["help"]) {
+            this.internalHelp = this.help;
+        }
+        if (!this.internalPlaceholder && this.placeholder && changes["placeholder"]) {
+            this.internalPlaceholder = this.placeholder;
+        }
+    }
     ngOnInit(): void {
-        
+
     }
     get helpId(): string {
         return `${this.id}-help`;
