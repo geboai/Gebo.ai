457a88341c9195412a7308bc0a19abbc1e683f48
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-11-02 22:31:23 +0100

    added GeboDocument by text initializer

--- gebo.core.parent/gebo.base.model/src/main/java/ai/gebo/document/model/GeboDocument.java
+++ gebo.core.parent/gebo.base.model/src/main/java/ai/gebo/document/model/GeboDocument.java
@@ -6,9 +6,6 @@
  * and https://mozilla.org/MPL/2.0/.
  * Copyright (c) 2025+ Gebo.ai 
  */
- 
- 
- 
 
 package ai.gebo.document.model;
 
@@ -24,91 +21,98 @@ import jakarta.el.MethodNotFoundException;
 import lombok.Data;
 
 /**
- * Represents a document with various attributes and methods to manipulate its content.
- * Includes functionality to manage different types of media fragments.
+ * Represents a document with various attributes and methods to manipulate its
+ * content. Includes functionality to manage different types of media fragments.
  * 
  * AI generated comments
  */
 @Data
-public final class GeboDocument implements JSonDuplicableObject<GeboDocument>{
-    
-    
-    // Unique identifier of the document.
-    private String id = null;
-    
-    // Identifier for the parent path in the virtual file system.
-    private String parentPathId = null;
-    
-    // URL associated with the document.
-    private String url = null;
-    
-    // Name of the document.
-    private String name = null;
-    
-    // File extension of the document.
-    private String extension = null;
-    
-    // MIME type of the document content.
-    private String contentType = null;
-    
-    // Date when the document was created.
-    private Date createdDate = null;
-    
-    // Date when the document was first processed.
-    private Date firstProcessedDate = null;
-    
-    // Date when the document was last processed.
-    private Date lastProcessedDate = null;
-    
-    // Size of the document in bytes.
-    private Long size = null;
-    
-    // Reference to the parent folder in the virtual file system.
-    private VFilesystemReference virtualParentFolderReference = null;
-    
-    // Reference to the file in the virtual file system.
-    private VFilesystemReference virtualFileReference = null;
-    
-    // Date when the original content of the document was last modified.
-    private Date originalContentModifiedDate = null;
-    
-    // Custom metadata associated with the document.
-    private Map<String, Object> customMetaData = new HashMap<>();
-    
-    // Additional attributes associated with the document.
-    private Map<String, Object> additionalAttributes = new HashMap<>();
-    
-    // List of text document fragments.
-    private List<GeboTextDocumentFragment> texts = new ArrayList<>();
-    
-    // List of image document fragments.
-    private List<GeboImageDocumentFragment> images = new ArrayList<>();
-    
-    // List of audio document fragments.
-    private List<GeboAudioDocumentFragment> audios = new ArrayList<>();
-    
-    // List of video document fragments.
-    private List<GeboVideoDocumentFragment> videos = new ArrayList<>();
-    
-    // List of media document fragments.
-    private List<GeboMediaDocumentFragment> media = new ArrayList<>();
-    
-    // List of cataloging labels.
-    private List<CatalogingLabel> catalogingLabel = new ArrayList<>();
+public final class GeboDocument implements JSonDuplicableObject<GeboDocument> {
 
-  
-    /**
-     * Attempts to add media to the document, throwing an exception since it's not implemented.
-     * 
-     * @param name             Name of the media.
-     * @param type              Type of the media.
-     * @param subtype           Subtype of the media.
-     * @param subtypeSuffix     Suffix for the media subtype.
-     * @param charset 
-     * @param dataAsByteArray   Byte array of the media data.
-     * @throws MethodNotFoundException when called, as implementation is missing.
-     */
-    public void tryAddMedia(String name, String type, String subtype, String subtypeSuffix, Charset charset, byte[] dataAsByteArray) {
-        //throw new MethodNotFoundException("The method tryAddMedia have to be implemented");
-    }
+	// Unique identifier of the document.
+	private String id = null;
+
+	// Identifier for the parent path in the virtual file system.
+	private String parentPathId = null;
+
+	// URL associated with the document.
+	private String url = null;
+
+	// Name of the document.
+	private String name = null;
+
+	// File extension of the document.
+	private String extension = null;
+
+	// MIME type of the document content.
+	private String contentType = null;
+
+	// Date when the document was created.
+	private Date createdDate = null;
+
+	// Date when the document was first processed.
+	private Date firstProcessedDate = null;
+
+	// Date when the document was last processed.
+	private Date lastProcessedDate = null;
+
+	// Size of the document in bytes.
+	private Long size = null;
+
+	// Reference to the parent folder in the virtual file system.
+	private VFilesystemReference virtualParentFolderReference = null;
+
+	// Reference to the file in the virtual file system.
+	private VFilesystemReference virtualFileReference = null;
+
+	// Date when the original content of the document was last modified.
+	private Date originalContentModifiedDate = null;
+
+	// Custom metadata associated with the document.
+	private Map<String, Object> customMetaData = new HashMap<>();
+
+	// Additional attributes associated with the document.
+	private Map<String, Object> additionalAttributes = new HashMap<>();
+
+	// List of text document fragments.
+	private List<GeboTextDocumentFragment> texts = new ArrayList<>();
+
+	// List of image document fragments.
+	private List<GeboImageDocumentFragment> images = new ArrayList<>();
+
+	// List of audio document fragments.
+	private List<GeboAudioDocumentFragment> audios = new ArrayList<>();
+
+	// List of video document fragments.
+	private List<GeboVideoDocumentFragment> videos = new ArrayList<>();
+
+	// List of media document fragments.
+	private List<GeboMediaDocumentFragment> media = new ArrayList<>();
+
+	// List of cataloging labels.
+	private List<CatalogingLabel> catalogingLabel = new ArrayList<>();
+
+	/**
+	 * Attempts to add media to the document, throwing an exception since it's not
+	 * implemented.
+	 * 
+	 * @param name            Name of the media.
+	 * @param type            Type of the media.
+	 * @param subtype         Subtype of the media.
+	 * @param subtypeSuffix   Suffix for the media subtype.
+	 * @param charset
+	 * @param dataAsByteArray Byte array of the media data.
+	 * @throws MethodNotFoundException when called, as implementation is missing.
+	 */
+	public void tryAddMedia(String name, String type, String subtype, String subtypeSuffix, Charset charset,
+			byte[] dataAsByteArray) {
+		// throw new MethodNotFoundException("The method tryAddMedia have to be
+		// implemented");
+	}
+
+	static public GeboDocument of(String text) {
+		GeboDocument document = new GeboDocument();
+		document.getTexts().add(GeboTextDocumentFragment.plainText(text));
+		return document;
+	}
 }
