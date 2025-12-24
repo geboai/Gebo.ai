4c11b1e260e4963db81b9ffa42c2877afca1eed1
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2025-12-24 13:45:38 +0100

    fix menu enable/disable access to company documents

--- gebo.ui/projects/gebo-ai-reusable-ui/src/lib/controls/chat-control/chat-input-shell.component.ts
+++ gebo.ui/projects/gebo-ai-reusable-ui/src/lib/controls/chat-control/chat-input-shell.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { GeboChatRequest, GeboChatResponse, GeboChatUserInfo, GUserChatInfo } from '@Gebo.ai/gebo-ai-rest-api';
 import { GeboAITranslationService } from '../field-translation-container/gebo-translation.service';
@@ -11,7 +11,7 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./chat-input-shell.component.scss'],
   standalone: false
 })
-export class GeboAIChatInputShellComponent implements OnInit {
+export class GeboAIChatInputShellComponent implements OnInit,OnChanges {
 
   @Input() interactions: any[] | null = null;
   @Input() formGroup!: FormGroup;
@@ -41,7 +41,30 @@ export class GeboAIChatInputShellComponent implements OnInit {
   @Output() messageSend = new EventEmitter<void>();
   @Output() deepSearchChatRequest: EventEmitter<GeboChatRequest> = new EventEmitter();
   @Output() deepsearchChatResponse: EventEmitter<GeboChatResponse> = new EventEmitter();
-
+  protected addBehaviorsMenu: MenuItem[] = [{
+    id: "UploadFileMenuItem",
+    icon: "pi pi-cloud-upload",
+    label: "Upload file(s)",
+    command: (event) => {
+      this.openUploadDialog();
+    }
+  }, {
+    id: "ChatWithDocsMenuItem",
+    icon: "pi pi-search",
+    label: "Browse/search company file(s)",
+    disabled:true,
+    command: (event) => {
+      this.openSelectDocsDialog();
+    }
+  }, {
+    id: "DeepSearchItem",
+    icon: "pi pi-deep-search",
+    label: "Deep search",
+    command: (event) => {
+      this.nextRequestMode = "deep-search";
+      this.chooseModeFormGroup.controls["nextRequestMode"].setValue(this.nextRequestMode);
+    }
+  }];
   protected currentDeepsearchChatRequest?: GeboChatRequest;
   protected nextRequestMode: "standard-chat" | "deep-search" = "standard-chat";
   protected requestTypeOptions: UIExistingText[] = [{ moduleId: "GeboAIReusableChatModel", entityId: "GeboAIChatInputShellComponent", componentId: "standard-chat", key: "label", fieldId: "label", text: "Chat", translation: "Chat" }, { moduleId: "GeboAIReusableChatModel", entityId: "GeboAIChatInputShellComponent", componentId: "deep-search", key: "label", fieldId: "label", text: "Deep search", translation: "Deep search" }];
@@ -61,35 +84,14 @@ export class GeboAIChatInputShellComponent implements OnInit {
     this.chooseModeFormGroup.controls["nextRequestMode"].valueChanges.subscribe(data => { this.nextRequestMode = data; });
     this.setStandardChatMode();
   }
+  
   private setStandardChatMode(): void {
     this.nextRequestMode="standard-chat";
     this.chooseModeFormGroup.controls["nextRequestMode"].setValue("standard-chat");
   }
 
   
-  addBehaviorsMenu: MenuItem[] = [{
-    id: "UploadFileMenuItem",
-    icon: "pi pi-cloud-upload",
-    label: "Upload file(s)",
-    command: (event) => {
-      this.openUploadDialog();
-    }
-  }, {
-    id: "ChatWithDocsMenuItem",
-    icon: "pi pi-search",
-    label: "Browse/search company file(s)",
-    command: (event) => {
-      this.openSelectDocsDialog();
-    }
-  }, {
-    id: "DeepSearchItem",
-    icon: "pi pi-deep-search",
-    label: "Deep search",
-    command: (event) => {
-      this.nextRequestMode = "deep-search";
-      this.chooseModeFormGroup.controls["nextRequestMode"].setValue(this.nextRequestMode);
-    }
-  }];
+  
   ngOnInit(): void {
     this.translationService.translateOnActualLanguage(this.requestTypeOptions).subscribe({
       next: (resources) => {
@@ -101,6 +103,15 @@ export class GeboAIChatInputShellComponent implements OnInit {
         }
       }
     });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+      if (changes["ragsystem"]) {
+        const item=this.addBehaviorsMenu.find(x=>x.id==="ChatWithDocsMenuItem");
+        if (item) {
+          item.disabled=!this.ragsystem;
+        }
+        this.addBehaviorsMenu=[...this.addBehaviorsMenu];
+      }
   }
   onSubmit() {
     this.onSendClick();
