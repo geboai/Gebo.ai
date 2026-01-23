6f8b8578bea42a2138f21c0c0dc21a1fb6993b3e
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2026-01-23 16:48:21 +0100

    deep search/chat support: full session consolidation including documents integration

--- gebo.architecture.parent/gebo.architecture.chat.abstraction.layer/src/main/java/ai/gebo/llms/chat/abstraction/layer/model/session/ShrinkedChatSessionState.java
+++ gebo.architecture.parent/gebo.architecture.chat.abstraction.layer/src/main/java/ai/gebo/llms/chat/abstraction/layer/model/session/ShrinkedChatSessionState.java
@@ -4,11 +4,16 @@ import ai.gebo.llms.chat.abstraction.layer.model.GUserChatInteractionsConsolidat
 import lombok.Data;
 
 @Data
-public class ShrinkedChatSessionState {
+public class ShrinkedChatSessionState implements ITokensCountable {
 	private CSSfRelevantShrinkedDocumentList relevantUploadedDocuments = new CSSfRelevantShrinkedDocumentList();
 	private CSSfRelevantShrinkedDocumentList relevantRagRetrievedDocuments = new CSSfRelevantShrinkedDocumentList();
 	private CSSfRelevantShrinkedDocumentList relevantLlmGeneratedDocuments = new CSSfRelevantShrinkedDocumentList();
 	private GUserChatInteractionsConsolidationData consolidatedInteractions = null;
-	
+
+	@Override
+	public int getTokensSize() {
+		return tokensSize(relevantLlmGeneratedDocuments, relevantRagRetrievedDocuments, relevantUploadedDocuments,
+				consolidatedInteractions);
+	}
 
 }
