841886447ea052a620cdc1466a173f8d1480207c
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2026-03-21 13:58:04 +0100

    UI commuication + reactive streaming, user notification during routing

---
+++ gebo.architecture.parent/gebo.architecture.chat.abstraction.layer/src/main/java/ai/gebo/llms/chat/abstraction/layer/llmexchange/model/ChatNotificationContent.java
@@ -0,0 +1,24 @@
+package ai.gebo.llms.chat.abstraction.layer.llmexchange.model;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatNotificationContent {
+	public static enum NotificationType {
+		INFO, DEBUG
+	}
+
+	@NotNull
+	private String code = null;
+	@NotNull
+	private String message = null;
+	private String icon = null;
+	private Long duration = null;
+	private NotificationType notificationType = NotificationType.INFO;
+}
