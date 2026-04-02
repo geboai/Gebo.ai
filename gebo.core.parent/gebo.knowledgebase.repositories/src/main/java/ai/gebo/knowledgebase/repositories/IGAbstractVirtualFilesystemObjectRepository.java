125d026289a2955650ecfe8b1a8f7a6493ea7250
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2026-04-02 12:45:48 +0200

    fix: list to stream

--- gebo.core.parent/gebo.knowledgebase.repositories/src/main/java/ai/gebo/knowledgebase/repositories/IGAbstractVirtualFilesystemObjectRepository.java
+++ gebo.core.parent/gebo.knowledgebase.repositories/src/main/java/ai/gebo/knowledgebase/repositories/IGAbstractVirtualFilesystemObjectRepository.java
@@ -172,7 +172,7 @@ public interface IGAbstractVirtualFilesystemObjectRepository<Type extends GAbstr
 
 	public List<Type> findByExtIntegrationCode(String extIntegrationCode);
 
-	public List<Type> findByExtIntegrationCodeIn(List<String> extIntegrationCodeList);
+	public Stream<Type> findByExtIntegrationCodeIn(List<String> extIntegrationCodeList);
 
 	/**
 	 * Deletes all objects by the parent project's code.
