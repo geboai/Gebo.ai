c34583e74ad8e636987c23e20debe3d5a160ab58
Paolo Zavalloni <p.zavalloni@vargroup.it>
2026-05-20 06:53:55 +0200

    new deep searches algorithm skeleton

---
+++ gebo.architecture.parent/gebo.architecture.chat.abstraction.layer/src/main/java/ai/gebo/llms/chat/abstraction/layer/services/IAISerchDataSource.java
@@ -0,0 +1,13 @@
+package ai.gebo.llms.chat.abstraction.layer.services;
+
+import java.util.stream.Stream;
+
+import org.springframework.ai.document.Document;
+
+import ai.gebo.llms.abstraction.layer.model.IChatRequestContext;
+import ai.gebo.llms.chat.pipelines.service.ISinkUIEmitter;
+import reactor.core.publisher.Flux;
+
+public interface IAISerchDataSource {
+	public Flux<Document> search(IChatRequestContext context, ISinkUIEmitter emitter, int topK);
+}
