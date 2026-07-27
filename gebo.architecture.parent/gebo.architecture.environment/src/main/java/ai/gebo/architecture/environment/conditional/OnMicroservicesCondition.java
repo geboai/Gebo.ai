6e78baee97c1f5992402d5f4d47cc8db19a917d5
Paolo Zavalloni <paolo.zavalloni@gebo.ai>
2026-07-27 22:01:17 +0200

    conditional architecture beans

---
+++ gebo.architecture.parent/gebo.architecture.environment/src/main/java/ai/gebo/architecture/environment/conditional/OnMicroservicesCondition.java
@@ -0,0 +1,21 @@
+package ai.gebo.architecture.environment.conditional;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import ai.gebo.architecture.environment.GeboApplicationArchitecture;
+import ai.gebo.architecture.environment.GeboApplicationArchitecture.ArchitectureType;
+
+public class OnMicroservicesCondition implements Condition {
+
+	@Override
+	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+		Map<String, GeboApplicationArchitecture> beans =
+				context.getBeanFactory().getBeansOfType(GeboApplicationArchitecture.class);
+		return beans.values().stream()
+				.anyMatch(a -> a.getArchitecture() == ArchitectureType.MICROSERVICES);
+	}
+}
